88764e04d3db1d0c17c4daa0530b452579ef0d28
wangding <408542507@qq.com>
2019-02-26 17:00:34 +0800

    rectangle

---
+++ rectangle.js
@@ -0,0 +1,16 @@
+$(function(){
+  var $width=$('#width'),
+      $height=$('#length'),
+      $btn=$('#calculate'),
+      $perimeter=$('#perimeter'),
+      $area=$('#area');
+  $btn.click(function(){
+  var w=Number($width.val()),
+      h=Number($height.val());
+  var p=2*(w+h),
+      a=w*h;
+  $perimeter.val(p);
+  $area.val(a);
+})
+
+});
