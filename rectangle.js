db2eb731d8ea8c4812b926bb40b168df66f1cc43
wangding <408542507@qq.com>
2019-02-26 21:09:56 +0800

    tag v0.1

--- rectangle.js
+++ rectangle.js
@@ -5,12 +5,11 @@ $(function(){
       $perimeter=$('#perimeter'),
       $area=$('#area');
   $btn.click(function(){
-  var w=Number($width.val()),
-      h=Number($height.val());
-  var p=2*(w+h),
-      a=w*h;
-  $perimeter.val(p);
-  $area.val(a);
-})
-
+    var w=Number($width.val()),
+        h=Number($height.val());
+    var p=2*(w+h),
+        a=w*h;
+    $perimeter.val(p);
+    $area.val(a);
+  })
 });
