26aa92db9dcd663a323b582fdefef26b8d72c007
tianyuanfang <1784634905@qq.com>
2019-03-12 16:52:21 +0800

    r

--- rectangle.js
+++ rectangle.js
@@ -1,18 +1,16 @@
 $(function(){
-    var $width=$('#width'),
+  var $width=$('#width'),
       $height=$('#length'),
       $btn=$('#calculate'),
       $perimeter=$('#perimeter'),
       $area=$('#area');
-$btn.click(function(){
+  $btn.click(function(){
       var w=Number($width.val()),
           h=Number($height.val());
     /*var p=2*(w+h),
      *         a=w*h;*/
     var rect=rectangle();
-        $perimeter.val(rect.perimeter(w,h));
-            $area.val(rect.area(w,h));
-              
-})
-
+    $perimeter.val(rect.perimeter(w,h));
+    $area.val(rect.area(w,h));
+  })
 });
--- test/calc.test.js
+++ test/calc.test.js
@@ -1,5 +1,3 @@
-var expect = chai.expect;
-
 describe('矩形面积函数的测试', function() {
   it('area(10, 5) = 50', function() {
     var r = rectangle();
