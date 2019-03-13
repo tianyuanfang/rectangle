fc201396f8a501af87f8bd33a63b44e3669c12b0
tianyuanfang <1784634905@qq.com>
2019-03-13 16:38:13 +0800

    v0.0

--- rectangle.js
+++ rectangle.js
@@ -5,8 +5,8 @@ $(function(){
       $perimeter=$('#perimeter'),
       $area=$('#area');
   $btn.click(function(){
-      var w=Number($width.val()),
-          h=Number($height.val());
+    var w=Number($width.val()),
+        h=Number($height.val());
     /*var p=2*(w+h),
      *         a=w*h;*/
     var rect=rectangle();
