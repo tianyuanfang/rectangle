944706ca7c9deb8fc5da17c52caa1044826a0a23
tianyuanfang <1784634905@qq.com>
2019-03-27 17:00:53 +0800

    v0.5

--- test/calc.test.js
+++ test/calc.test.js
@@ -1,12 +1,13 @@
 var expect = chai.expect;
+
 describe('矩形面积函数的测试', function() {
   it('area(10, 5) = 50', function() {
-    var r = rectangle();
-    expect(r.area(10, 5)).to.be.equal(50);
+    var r = Rectangle(10,5);
+    expect(r.area()).to.be.equal(50);
   });
 
   it('area("10", "5") = 50', function() {
-    var r = rectangle();
-    expect(r.area("10", "5")).to.be.equal(50);
+    var r = rectangle("10","5");
+    expect(r.area().to.be.equal(50);
   });
 });
