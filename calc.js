1c21d85a1a0dab5915eba66f47061e81dc6c7c0d
tianyuanfang <1784634905@qq.com>
2019-03-27 16:57:52 +0800

    v0.5

--- calc.js
+++ calc.js
@@ -1,11 +1,46 @@
-function rectangle() {
-  return {
-    'perimeter' : function(w, h) {
-      return 2 * (Number(w) + Number(h));
-    },
-    'area': function(w, h) {
-      return Number(w) * Number(h);
-    }
+function Rectangle(width, height) {
+  var w = Number(width),
+      h = Number(height);
+
+  this.area = function() {
+    return w * h;
   };
+
+  this.perimeter = function() {
+    return 2 * (w + h);
+  };
+}
+
+/**
+ * 对数据进行合法性校验
+ *
+ * @param msg 被验证的信息
+ * @returns result 有两个字段
+ *          isOK boolean 验证通过为 true，验证不通过为 false
+ *          reason 验证不通过的理由
+ */
+function validate(data) {
+  var result = {
+    isOK: false,
+    reason: ''
+  };
+
+  if(data === '') {
+    result.reason = '不能为空！';
+    return result;
+  }
+
+  if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data)) {
+    result.reason = '必须是数值';
+    return result;
+  }
+
+  if(Number(data) < 0) {
+    result.reason = '必须大于零';
+    return result;
+  }
+
+  result.isOK = true;
+  return result;
 }
 
--- rectangle.js
+++ rectangle.js
@@ -1,16 +1,44 @@
-$(function(){
-  var $width=$('#width'),
-      $height=$('#length'),
-      $btn=$('#calculate'),
-      $perimeter=$('#perimeter'),
-      $area=$('#area');
-  $btn.click(function(){
-    var w=Number($width.val()),
-        h=Number($height.val());
-    /*var p=2*(w+h),
-     *         a=w*h;*/
-    var rect=rectangle();
-    $perimeter.val(rect.perimeter(w,h));
-    $area.val(rect.area(w,h));
+$(function() {
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area'),
+      $widthValidate = $('#width-validate'),
+      $heightValidate = $('#height-validate'),
+      isPassValidate = false;
+
+  $width.focusout(function() {
+    var result = validate($width.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $widthValidate.html('宽度' + result.reason);
+      $width.select();
+    } else {
+      $widthValidate.html('');
+    }
+  });
+
+  $height.focusout(function() {
+    var result = validate($height.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $heightValidate.html('高度' + result.reason);
+      $height.select();
+    } else {
+      $heightValidate.html('');
+    }
+  });
+
+  $btnCal.click(function(){
+    if(!isPassValidate) return;
+
+    var w = $width.val(),
+        h = $height.val();
+
+    var r = new Rectangle(w, h);
+
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
   });
 });
